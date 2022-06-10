3258b28a40de932bf8599098896d37065755f990
Mochamad Iqbal <iqbalvandame2134@gmail.com>
2022-06-10 19:57:35 +0700

    add crud for table category

--- app/online-course/routes.js
+++ app/online-course/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const courseAdmin = require('./controllers/course-admin');
+const categoryAdmin = require('./controllers/category-admin');
 const coursePublic = require('./controllers/course-public');
 
 const router = express.Router();
@@ -11,6 +12,11 @@ router.post('/admin/courses', courseAdmin.create);
 router.get('/admin/courses/:id', courseAdmin.show);
 router.put('/admin/courses/:id', courseAdmin.update);
 router.delete('/admin/courses/:id', courseAdmin.destroy);
+router.get('/admin/categories', categoryAdmin.all);
+router.post('/admin/categories', categoryAdmin.create);
+router.get('/admin/categories/:id', categoryAdmin.show);
+router.put('/admin/categories/:id', categoryAdmin.update);
+router.delete('/admin/categories/:id', categoryAdmin.destroy);
 
 // public
 router.get('/public/courses', coursePublic.all);
